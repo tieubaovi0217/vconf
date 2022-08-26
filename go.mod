1c111f21bb2dd77590f028d4d1a378ca6763de18
tieubaovi0217 <nguyenvitieubao0217@gmail.com>
2022-08-26 23:56:38 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tieubaovi0217/vconf
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tieubaovi0217/vconf/util"
+)
+
+func main() {
+	fmt.Println(util.Add(1, 2))
+	fmt.Println("Hello World")
+}
